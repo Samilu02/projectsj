64617f5a7dc5a340496370fea4cc66e5ca942155
Samilu02 <133858501+Samilu02@users.noreply.github.com>
2023-06-10 06:18:26 +0200

    iii

--- src/main/java/ch/zhaw/projectsj/mongodb/MongoDBExport.java
+++ src/main/java/ch/zhaw/projectsj/mongodb/MongoDBExport.java
@@ -9,10 +9,29 @@ import org.bson.Document;
 public class MongoDBExport {
 
     public static void main(String[] args) {
+        try {
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase database = mongoClient.getDatabase("yourDatabase");
         MongoCollection<Document> collection = database.getCollection("yourCollection");
 
-        // Query your data here and export it to CSV files
-    }
+        List<Document> documents = new ArrayList<>();
+for (Prescription prescription : prescriptionRepository.findAll()) {
+    Document doc = new Document("_id", prescription.getId())
+        .append("name", prescription.getName());
+        // add other prescription attributes here
+    documents.add(doc);
+}
+collection.insertMany(documents);
+
+// Query MongoDB to get the list of nodes and edges
+FindIterable<Document> nodes = collection.find();
+for (Document node : nodes) {
+    System.out.println(node.toJson());
+}
+mongoClient.close();
+// Query your data here and export it to CSV files
+}catch (Exception e) {
+    e.printStackTrace();
+}
+ }
 }
